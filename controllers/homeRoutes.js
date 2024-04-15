a0dd1d1d2e34f7c7f621c2739d742231b86017fd
Mark Lewis <mlewis89@gmail.com>
2024-04-15 17:14:31 +1000

    server rendering handlesbars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
-const { BlogPost } = require('../../models');
-const withAuth = require('../../utils/auth');
+const {User, BlogPost } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
-  try {
-    
+router.get('/', async (req, res) => {
+    try {
+    const PostData = await BlogPost.findAll().catch((err) => { 
+        res.json(err);
+      });
+        const posts = PostData.map((dish) => dish.get({ plain: true }));
+        res.render('all', { posts });
+      
+  
     
   } catch (err) {
     res.status(400).json(err);
   }
-});
+}); 
 
 module.exports = router;
