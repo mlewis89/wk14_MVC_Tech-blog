e230afc8b34551629970a91e140b9f928ca12308
Mark Lewis <mlewis89@gmail.com>
2024-04-15 11:28:51 +1000

    model index.js

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const BlogPost = require('./BlogPost');
+const Comment = require('./Comment');
+
+User.hasMany(BlogPost, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+BlogPost.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+module.exports = { User, Project };
