dd4496ba0eba9e3388443ffbfd417077ba0e2647
Mark Lewis <mlewis89@gmail.com>
2024-04-17 21:07:04 +1000

    login and user dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const { Model } = require('sequelize');
 const { User, BlogPost, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/',withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const PostData = await BlogPost.findAll({
       include: [{ model: User }]
@@ -14,6 +15,35 @@ router.get('/',withAuth, async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+router.get('/login', async (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    //const userData = await User.findByPk(req.session.user_id);
+    //console.log(userData);
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/dashboard', async (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    const userData = await User.findByPk(req.session.user_id,{
+      include: [{ model: BlogPost }, { model: Comment }],
+      attributes: { exclude: ['password'] },
+    });
+
+    console.log(userData);
+    const user = userData.get({ plain: true });
+    res.render('dashboard',{user});
+    return;
+  }
+
+  res.redirect('login');
 });
 
 module.exports = router;
