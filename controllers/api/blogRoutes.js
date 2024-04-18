165c6d6d28c342533f73f8a769fb376f6aaedc7d
Mark Lewis <mlewis89@gmail.com>
2024-04-18 14:06:17 +1000

    Add Comments

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -4,9 +4,33 @@ const withAuth = require('../../utils/auth');
 
 //WHEN I visit the site for the first time
 //THEN I am presented with the homepage, which includes existing blog posts if any have been posted; navigation links for the homepage and the dashboard; and the option to log in
-router.post('/', withAuth, async (req, res) => {
+
+router.post('/post', withAuth, async (req, res) => {
   try {
     
+    //console.log(req.body, req.session.user_id);
+    const newPost = await BlogPost.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    console.log('new Post Added', newPost)
+    res.status(200).json(newPost);
+    
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/comment', withAuth, async (req, res) => {
+  try {
+    
+    //console.log(req.body, req.session.user_id);
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    console.log('newComment Added', newComment)
+    res.status(200).json(newComment);
     
   } catch (err) {
     res.status(400).json(err);
--- public/js/comment.js
+++ public/js/comment.js
@@ -3,30 +3,25 @@ const CommentSubmitHandler = async (event) => {
 
   // Collect values from the comment form
   const commentText = document.querySelector('#new-comment').value.trim();
-  //const currentUserId = req.session.user_id;
   const currentPostId = event.target.closest('.post').getAttribute('data-post-id');
  
-  const comment = {
-    content: commentText,
-    //user_id: currentUserId,
-    post_id: currentPostId
-  };
-
-  if(comment) {
+if(commentText && currentPostId)
+{
     // Send a POST request to the API endpoint
     const response = await fetch('/api/blog/comment', {
       method: 'POST',
-      body: JSON.stringify({ comment }),
+      body: JSON.stringify({content: commentText, post_id: currentPostId }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       // If successful, redirect the browser to the /dashboard page
-      document.location.replace('/dashboard');
+      document.location.replace(`/posts/${currentPostId}`);
     } else {
       alert(response.statusText);
     }
   }
+
 };
 
 
